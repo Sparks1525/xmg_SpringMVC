21f876774b73c9d75f772183651b7b2d2671bb53
sparks1525 <shi15259515212@gmail.com>
2019-03-16 16:59:07 +0800

    update

--- _04/src/my/demo/controller/HelloWorldController.java
+++ _04/src/my/demo/controller/HelloWorldController.java
@@ -1,15 +1,19 @@
 package my.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+@Controller
+public class HelloWorldController {
 
-public class HelloWorldController implements Controller {
-    @Override
-    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        System.out.println("hello World!");
-        return null;
+    @RequestMapping("/hello")
+    public ModelAndView test1(){
+
+        System.out.println("Hello World!");
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("demo.html");
+        return mv;
     }
+
 }
