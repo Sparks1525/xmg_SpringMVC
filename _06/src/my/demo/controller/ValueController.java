be9c2e58630ce0073b3231993c84aa4daecc04eb
sparks1525 <shi15259515212@gmail.com>
2019-04-08 15:49:39 +0800

    update

--- _06/src/my/demo/controller/ValueController.java
+++ _06/src/my/demo/controller/ValueController.java
@@ -53,9 +53,9 @@ public class ValueController {
 
     //前端传入后端的parameter,后端接收可以在参数上打@RequestParam("前端parameter名")标签来唯一标识
     @RequestMapping("/value4")
-    public ModelAndView value4(@RequestParam("password") String password,@RequestParam("name") String userName){
+    public ModelAndView value4(@RequestParam("password") String password, @RequestParam("name") String userName) {
         System.out.println("ValueController.value4()");
-        User u = new User(userName,password);
+        User u = new User(userName, password);
         System.out.println(u);
         ModelAndView mv = new ModelAndView("input.jsp");
         return mv;
@@ -63,7 +63,7 @@ public class ValueController {
 
     //前端传入后端的parameter,后端参数可以使用对象来接收,值传入对象是根据parameter名和对象setXXX值,也就是属性注入的方式
     @RequestMapping("/value5")
-    public ModelAndView value5(User u){
+    public ModelAndView value5(User u) {
         System.out.println("value5");
         System.out.println(u);
         ModelAndView mv = new ModelAndView("input.jsp");
@@ -72,7 +72,7 @@ public class ValueController {
 
     //前端传入后端的parameter,后端参数可以使用对象来接收,值传入对象是根据parameter名和对象setXXX值,也就是属性注入的方式
     @RequestMapping("/value6")
-    public ModelAndView value6(User u){
+    public ModelAndView value6(User u) {
         System.out.println("value6");
         System.out.println(u);
         ModelAndView mv = new ModelAndView("input.jsp");
@@ -81,7 +81,7 @@ public class ValueController {
 
     //地址栏传參  localhost/delete/5
     @RequestMapping("/delete/{id}")
-    public ModelAndView value7(@PathVariable("id") Long id){
+    public ModelAndView value7(@PathVariable("id") Long id) {
         System.out.println("delete");
         System.out.println(id);
         ModelAndView mv = new ModelAndView("input.jsp");
